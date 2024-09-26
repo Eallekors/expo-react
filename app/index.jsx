02f278f3409a6ffe71dfc0e68da8ff2187a5de00
Eallekors <erkxalle@gmail.com>
2024-09-26 18:05:17 +0300

    Profile

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Aurora!</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'blue'}}>Go to Profile</Link>
     </View>
   );
 }
